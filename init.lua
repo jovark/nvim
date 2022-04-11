9e6e58c47f48499489ae3fc3ccb09bff66868c11
Joonatan <joonatanvark@gmail.com>
2022-04-11 21:12:49 +0300

    lsp

--- init.lua
+++ init.lua
@@ -3,4 +3,5 @@ require "user.keymaps"
 require "user.plugins"
 require "user.cmp"
 require "user.colorscheme"
+require "user.lsp"
 
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -56,12 +56,16 @@ return packer.startup(function(use)
     use "hrsh7th/cmp-path" -- path completions
     use "hrsh7th/cmp-cmdline" -- cmdline completions
     use "saadparwaiz1/cmp_luasnip" -- snippet completions
-    -- use "hrsh7th/cmp-nvim-lsp"
+    use "hrsh7th/cmp-nvim-lsp"
+    use "hrsh7th/cmp-nvim-lua"
 
     -- snippets
     use "L3MON4D3/LuaSnip" --snippet engine
     use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
+    -- LSP
+    use "neovim/nvim-lspconfig" -- enable LSP
+    use "williamboman/nvim-lsp-installer" -- simple to use language server installer
     
     -- At the end after all the plugins
     if PACKER_BOOTSTRAP then
