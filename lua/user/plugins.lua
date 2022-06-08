6e14076b08cb3ee636bfba93d8595307ecd35ac4
joonatan <joonatan@mydesktop.localdomain>
2022-06-08 19:17:08 +0300

    added tabnine

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -70,6 +70,7 @@ return packer.startup(function(use)
     use "saadparwaiz1/cmp_luasnip" -- snippet completions
     use "hrsh7th/cmp-nvim-lsp"
     use "hrsh7th/cmp-nvim-lua"
+    use { 'tzachar/cmp-tabnine', run = './install.sh', requires = 'hrsh7th/nvim-cmp' }
 
     -- snippets
     use "L3MON4D3/LuaSnip" --snippet engine
