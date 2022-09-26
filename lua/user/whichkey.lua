c7d7f3a47dc0cd69cdfe4ab21e7bff7abd5ca09a
joonatan <57330540+jovark@users.noreply.github.com>
2022-09-26 16:12:45 +0300

    udpated keybinds

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -158,6 +158,12 @@ local mappings = {
         h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
         v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
     },
+
+    T = {
+        name = "Treesitter",
+        h = { "<cmd>TSHighlightCapturesUnderCursor<cr>", "Highlight" },
+        p = { "<cmd>TSPlaygroundToggle<cr>", "Playground" },
+    },
 }
 
 local vopts = {
